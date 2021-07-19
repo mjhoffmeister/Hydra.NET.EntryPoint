143f549e0444a0e0ec5eabf5d0e17cbc0d31b8c5
Matt Hoffmeister <matt.hoffmeister@gmail.com>
2021-07-19 08:47:18 -0500

    Change icon hints to constants

--- Hydra.NET.EntryPoint/IconHint.cs
+++ Hydra.NET.EntryPoint/IconHint.cs
@@ -5,28 +5,28 @@
     /// </summary>
     public static class IconHint
     {
-        public static string Add = "Add";
+        public const string Add = "Add";
 
-        public static string CheckBox = "CheckBox";
+        public const string CheckBox = "CheckBox";
 
-        public static string CheckBoxOutlineBlank = "CheckBoxOutlineBlank";
+        public const string CheckBoxOutlineBlank = "CheckBoxOutlineBlank";
 
-        public static string DateRange = "DateRange";
+        public const string DateRange = "DateRange";
 
-        public static string Delete = "Delete";
+        public const string Delete = "Delete";
 
-        public static string Edit = "Edit";
+        public const string Edit = "Edit";
 
-        public static string Event = "Event";
+        public const string Event = "Event";
 
-        public static string Notifications = "Notifications";
+        public const string Notifications = "Notifications";
 
-        public static string People = "People";
+        public const string People = "People";
 
-        public static string ShowChart = "ShowChart";
+        public const string ShowChart = "ShowChart";
 
-        public static string SwapHorizontalCircle = "SwapHorizontalCircle";
+        public const string SwapHorizontalCircle = "SwapHorizontalCircle";
 
-        public static string QueueMusic = "QueueMusic";
+        public const string QueueMusic = "QueueMusic";
     }
 }
